37cbf6d362bc023f7ec7119087a71ee1c42ff6d4
klaudia tamburi <ktamburi22@epoka.edu.al>
2025-05-06 15:21:34 +0200

    tasks check

--- app/Models/Morningnews.php
+++ app/Models/Morningnews.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Morningnews extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'morning_news';
+
     use HasFactory;
 
     protected $fillable = ['title', 'news_text'];
