533f391d657cffbbc0e680134835ce3912d4ef4e
klaudia tamburi <ktamburi22@epoka.edu.al>
2025-05-06 15:31:49 +0200

    fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,7 +33,7 @@ class UserController extends Controller
     {
         // TASK: find a user by $name and $email
         //   if not found, create a user with $name, $email and random password
-        $user = firstOrCreate(
+        $user = User::firstOrCreate(
             ['name' => $name, 'email' => $email],
             ['password' => Hash::make(Str::random(12))]
         );
